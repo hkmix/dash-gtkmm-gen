d0562c541e490f2ad9381ba5b6d7929a08a0b714
Jack Zhou <jack@zzh.ca>
2016-07-12 03:10:27 -0400

    Add missing glibmm and giomm classes

--- gen_docset.py
+++ gen_docset.py
@@ -17,55 +17,56 @@ cur.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TE
 cur.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
 
 docpath = 'gtkmm.docset/Contents/Resources/Documents'
-docbase = 'gtkmm/stable'
+docbases = ['gtkmm/stable', 'glibmm/stable']
 
-for file in os.listdir(os.path.join(docpath, docbase)):
-    if not file.endswith('.html'):
-        continue
+for docbase in docbases:
+    for file in os.listdir(os.path.join(docpath, docbase)):
+        if not file.endswith('.html'):
+            continue
 
-    # Regex for title
-    title_re = re.compile('gtkmm: *')
+        # Regex for title
+        title_re = re.compile('(gtkmm|glibmm): *')
 
-    # Regexes for file prefixes
-    class_re = re.compile('class.*\.html')
-    category_re = re.compile('group.*\.html')
-    namespace_re = re.compile('namespace.*\.html')
+        # Regexes for file prefixes
+        class_re = re.compile('(class|struct).*\.html')
+        category_re = re.compile('group.*\.html')
+        namespace_re = re.compile('namespace.*\.html')
 
-    # Regexes for headers
-    class_remove = re.compile('(gtkmm:|Class *\w* Reference)')
-    category_remove = re.compile('gtkmm:')
-    namespace_remove = re.compile('(gtkmm:|Namespace Reference)')
+        # Regexes for headers
+        class_remove = re.compile('(gtkmm:|glibmm:|(Class|Struct) *\w* Reference)')
+        category_remove = re.compile('(gtkmm|glibmm):')
+        namespace_remove = re.compile('(gtkmm:|glibmm:|Namespace Reference)')
 
-    # Create BS object and strip <title> prefix if there is one
-    soup = bs(open(os.path.join(docpath, docbase, file)).read(), 'html5lib')
-    title = soup.find('title')
-    if (title_re.match(title.text.strip())):
-        title.string = title_re.sub('', title.text.strip()).strip()
-        with open(os.path.join(docpath, docbase, file), 'wb') as out_file:
-            out_file.write(soup.prettify('utf-8'))
+        # Create BS object and strip <title> prefix if there is one
+        soup = bs(open(os.path.join(docpath, docbase, file)).read(), 'html5lib')
+        title = soup.find('title')
+        if (title_re.match(title.text.strip())):
+            title.string = title_re.sub('', title.text.strip()).strip()
+            with open(os.path.join(docpath, docbase, file), 'wb') as out_file:
+                out_file.write(soup.prettify('utf-8'))
 
-    header = soup.find('h1').text.strip()
+        header = soup.find('h1').text.strip()
 
-    # Class
-    if class_re.match(file):
-        name = class_remove.sub('', header).strip()
-        if name is not None:
-            print("Found class:", name)
-            cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'Class', os.path.join(docbase, file)))
+        # Class
+        if class_re.match(file):
+            name = class_remove.sub('', header).strip()
+            if name is not None:
+                print("Found class:", name)
+                cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'Class', os.path.join(docbase, file)))
 
-    # Category
-    elif category_re.match(file):
-        name = category_remove.sub('', header).strip()
-        if name is not None:
-            print("Found category:", name)
-            cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'Category', os.path.join(docbase, file)))
+        # Category
+        elif category_re.match(file):
+            name = category_remove.sub('', header).strip()
+            if name is not None:
+                print("Found category:", name)
+                cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'Category', os.path.join(docbase, file)))
 
-    # Namespace
-    elif namespace_re.match(file):
-        name = namespace_remove.sub('', header).strip()
-        if name is not None:
-            print("Found namespace:", name)
-            cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'Category', os.path.join(docbase, file)))
+        # Namespace
+        elif namespace_re.match(file):
+            name = namespace_remove.sub('', header).strip()
+            if name is not None:
+                print("Found namespace:", name)
+                cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'Category', os.path.join(docbase, file)))
 
 db.commit()
 db.close()
--- scrape.sh
+++ scrape.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+rm -rf developer.gnome.org
+rm -rf gtkmm.docset/Contents/Resources/Documents/*
+
 wget \
   --recursive \
   --no-clobber \
@@ -9,3 +12,16 @@ wget \
   --domains developer.gnome.org \
   --no-parent \
   https://developer.gnome.org/gtkmm/stable/
+
+wget \
+  --recursive \
+  --no-clobber \
+  --page-requisites \
+  --html-extension \
+  --convert-links \
+  --domains developer.gnome.org \
+  --no-parent \
+  https://developer.gnome.org/glibmm/stable/
+
+mv developer.gnome.org/* gtkmm.docset/Contents/Resources/Documents
+rm -rf developer.gnome.org
